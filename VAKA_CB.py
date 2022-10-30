7fb19267f8f5925c6c7f560a7b4772f00ddf2cc6
shimaa-taha <94011310+shimaa-taha@users.noreply.github.com>
2022-10-30 19:08:22 +0200

    Update VAKA_CB.py

--- VAKA_CB.py
+++ VAKA_CB.py
@@ -3,7 +3,6 @@
 
 # In[34]:
 
-
 get_ipython().system('pip install pyopenms')
 import pyopenms
 from pyopenms import *
@@ -12,32 +11,7 @@ seq = AASequence.fromString("VAKA")     # create AASequence object
 seq_Formula = seq.getFormula()
 print("Peptide", seq, "has molecular formula", seq_Formula)
 x=0
-cnt=1
-# print coarse isotope distribution
-isotopes = seq_Formula.getIsotopeDistribution( CoarseIsotopePatternGenerator(4) )
-for iso in isotopes.getContainer():
-    print ("Iso ",cnt ," ", iso.getMZ(), "has abundance", iso.getIntensity()*100, "%")
-    cnt=cnt+1
-    x=x+iso.getMZ()
-print("mass of V + mass of A + mass of K + mass of A = ",x)
-mz = seq.getMZ(1)+seq.getMZ(2)+seq.getMZ(3)+seq.getMZ(4)
-print("MZ = ",mz)
-
-
-# In[26]:
-
-
-
-
-
-# In[35]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+for aa in seq:
+    x=x+ aa.getMonoWeight()# every element alone 
+print("mass of VAKA ",seq.getMonoWeight())#mass of VAKA
+print ("every element alone ",x)
